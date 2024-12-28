958338e96524884821307be40bd2db2ba0d6a127
Marcus Vinicius <marcus.santos@hndlabs.com.br>
2024-12-28 16:45:17 -0300

    FORMATER PAGE MENU

--- src/components/MenuGroup/index.tsx
+++ src/components/MenuGroup/index.tsx
@@ -6,81 +6,76 @@ import IconMapper from '../IconMapper';
 import { LinksGroupProps } from '@/types/menu';
 
 export function MenuGroup({ icon, label, initiallyOpened, links, active, setActive }: LinksGroupProps) {
-  const hasLinks = Array.isArray(links);
-  const [opened, setOpened] = useState(initiallyOpened || false);
+	const hasLinks = Array.isArray(links);
+	const [opened, setOpened] = useState(initiallyOpened || false);
 
-  // Itens de links para renderizar
-  const items = (hasLinks ? links : []).map((link) => (
-    <Text<'a'>
-      component="a"
-      data-active={active.sub === link.label || undefined}
-      className={classes.link}
-      href={link.link}
-      key={link.label}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive({ main: label, sub: link.label });
-      }}
-    >
-      {link.label}
-    </Text>
-  ));
+	const items = (hasLinks ? links : []).map((link) => (
+		<Text<'a'>
+		component="a"
+		data-active={active.sub === link.label || undefined}
+		className={classes.link}
+		href={link.link}
+		key={link.label}
+		onClick={(event) => {
+			event.preventDefault();
+			setActive({ main: label, sub: link.label });
+		}}
+		>
+		{link.label}
+		</Text>
+	));
 
-  // Efeito para selecionar o primeiro link quando o grupo é aberto e nenhum item estiver selecionado
-  useEffect(() => {
-    if (opened && hasLinks && active.main === label && !active.sub) {
-      // Seleciona o primeiro link apenas se o grupo estiver aberto e não houver link ativo
-      setActive({ main: label, sub: links[0].label });
-    }
-  }, [opened, links, active.sub, label, active.main, setActive]);
+	useEffect(() => {
+		if (opened && hasLinks && active.main === label && !active.sub) {
+		// Seleciona o primeiro link apenas se o grupo estiver aberto e não houver link ativo
+		setActive({ main: label, sub: links[0].label });
+		}
+	}, [opened, links, active.sub, label, active.main, setActive]);
 
-  const handleGroupClick = () => {
+	const handleGroupClick = () => {
+		if(!hasLinks){
+			setActive({ main: label, sub: ''});
+		}else{
+			setOpened(!opened);
+		}
+	};
 
-
-
-	if(!hasLinks){
-		setActive({ main: label, sub: ''});
-	}else{
-		setOpened(!opened);
+	return (
+		<>
+		<UnstyledButton
+			onClick={handleGroupClick}
+			className={classes.control}
+			data-active={label === active.main || undefined}
+		>
+			<Group justify="space-between" gap={0}>
+				<a
+					className={classes.linkSingle}
+					href="#"
+					key={label}
+					data-active={label === active.main || undefined}
+					onClick={(event) => {
+					event.preventDefault();
+					const subActive = hasLinks && opened ? links[0].label : '';
+					setActive({
+						main: label,
+						sub: subActive,
+					});
+					}}
+				>
+					<IconMapper iconName={icon} className={classes.linkIcon} stroke={1.5} />
+					<span>{label}</span>
+				</a>
+			{hasLinks && (
+				<IconChevronRight
+				className={classes.chevron}
+				stroke={1.5}
+				size={16}
+				style={{ transform: opened ? 'rotate(-90deg)' : 'none' }}
+				/>
+			)}
+			</Group>
+		</UnstyledButton>
+		{hasLinks ? <Collapse in={opened}>{items}</Collapse> : null}
+		</>
+	);
 	}
-  };
-
-  return (
-    <>
-      <UnstyledButton
-        onClick={handleGroupClick}
-        className={classes.control}
-        data-active={label === active.main || undefined}
-      >
-        <Group justify="space-between" gap={0}>
-          <a
-            className={classes.linkSingle}
-            href="#"
-            key={label}
-            data-active={label === active.main || undefined}
-            onClick={(event) => {
-              event.preventDefault();
-              const subActive = hasLinks && opened ? links[0].label : '';
-              setActive({
-                main: label,
-                sub: subActive,
-              });
-            }}
-          >
-            <IconMapper iconName={icon} className={classes.linkIcon} stroke={1.5} />
-            <span>{label}</span>
-          </a>
-          {hasLinks && (
-            <IconChevronRight
-              className={classes.chevron}
-              stroke={1.5}
-              size={16}
-              style={{ transform: opened ? 'rotate(-90deg)' : 'none' }}
-            />
-          )}
-        </Group>
-      </UnstyledButton>
-      {hasLinks ? <Collapse in={opened}>{items}</Collapse> : null}
-    </>
-  );
-}
