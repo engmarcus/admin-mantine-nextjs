d0ad834c2174c1159b2098ba50ca597f1853ed95
Marcus Vinicius <marcus.santos@hndlabs.com.br>
2025-01-01 17:56:54 -0300

    Upgrades

--- src/store/sidebarStore.ts
+++ src/store/sidebarStore.ts
@@ -2,16 +2,16 @@ import { MenuStructure } from '@/types/menu';
 import { create } from 'zustand';
 
 interface SidebarState {
-  menus: MenuStructure | null;
-  clickOpen: boolean;
-  isOpen: boolean;
-  mobileOpen: boolean;
-  breakPoint: string;
-  toggleSidebar: (isMobile: boolean) => void;
-  setOpen: (value: boolean) => void;
-  handleMouseEnter: () => void;
-  handleMouseLeave: () => void;
-  setMenus: (menus: MenuStructure) => void;
+	menus: MenuStructure | null;
+	clickOpen: boolean;
+	isOpen: boolean;
+	mobileOpen: boolean;
+	breakPoint: string;
+	toggleSidebar: (isMobile: boolean) => void;
+	setOpen: (value: boolean) => void;
+	handleMouseEnter: () => void;
+	handleMouseLeave: () => void;
+	setMenus: (menus: MenuStructure) => void;
 }
 
 const useSidebarStore = create<SidebarState>((set) => ({
@@ -32,28 +32,28 @@ const useSidebarStore = create<SidebarState>((set) => ({
 		  clickOpen: !state.clickOpen,
 		  isOpen: !state.clickOpen,
 		};
-	  }),
+	}),
 	setOpen: (value) =>
 	  set((state) => ({
 		isOpen: value,
 		clickOpen: state.clickOpen ? state.clickOpen : value,
-	  })),
+	})),
 	handleMouseEnter: () =>
 	  set((state) => {
 		if (!state.clickOpen && !state.isOpen) {
 		  return { isOpen: true };
 		}
 		return {};
-	  }),
+	}),
 	handleMouseLeave: () =>
 	  set((state) => {
 		if (!state.clickOpen && state.isOpen) {
 		  return { isOpen: false };
 		}
 		return {};
-	  }),
+	}),
 	setMenus: (menus) => set({ menus }),
-  }));
+}));
 
 
 export default useSidebarStore;
